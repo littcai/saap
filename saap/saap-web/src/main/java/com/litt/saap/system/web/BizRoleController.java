2ecde9a3497bce9fa977247e3e81a3211f077979
cwang <313798472@qq.com>
2015-01-06 14:25:36 +0800

    删除数据授权的冗余实体

---
+++ saap/saap-web/src/main/java/com/litt/saap/system/web/BizRoleController.java
@@ -0,0 +1,204 @@
+package com.litt.saap.system.web;
+
+import javax.annotation.Resource;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.litt.saap.common.vo.LoginUserVo;
+import com.litt.saap.core.common.SaapConstants.RoleStatus;
+import com.litt.saap.core.module.tenant.config.TenantTypeConfigManager;
+import com.litt.saap.core.web.util.LoginUtils;
+import com.litt.saap.system.biz.ITenantBizService;
+import com.litt.saap.system.po.Role;
+import com.litt.saap.system.service.IRoleService;
+import com.litt.saap.system.vo.PermissionTreeVo;
+import com.litt.saap.system.vo.TenantVo;
+import com.litt.core.dao.page.IPageList;
+import com.litt.core.common.Utility;
+import com.litt.core.web.util.WebUtils;
+import com.litt.core.dao.ql.PageParam;
+import com.litt.core.exception.NotLoginException;
+import com.litt.core.module.annotation.Func;
+import com.litt.core.web.mvc.action.BaseController;
+
+/**
+ * 
+ * Roles controller.
+ * <pre><b>Description：</b>
+ *    Role and privilege Management
+ * </pre>
+ * 
+ * @author <a href="mailto:littcai@hotmail.com">Bob.cai</a>
+ * @since 2014-02-27
+ * @version 1.0
+ */
+@Controller
+public class BizRoleController extends BaseController
+{
+	private final static Logger logger = LoggerFactory.getLogger(BizRoleController.class);
+
+	@Resource
+	private IRoleService roleService;
+	@Resource
+	private ITenantBizService tenantBizService;
+	
+	/**
+	 * default page.
+	 * 
+	 * @param request the request
+	 * @param respnose the respnose
+	 * 
+	 * @return ModelAndView
+	 */	
+	@Func(funcCode="query", moduleCode="tenant.bizRole", enableLog=false) 
+	@RequestMapping 
+	public ModelAndView index(WebRequest request, ModelMap modelMap) throws NotLoginException
+	{	
+		LoginUserVo loginUserVo = (LoginUserVo)super.getLoginVo();
+		
+		//get params from request		
+		String name = request.getParameter("name");
+				
+		//package the params
+		PageParam pageParam = WebUtils.getPageParam(request);
+		pageParam.addCond("tenantId", loginUserVo.getTenantId());
+		pageParam.addCond("name", name);	
+		pageParam.addCond("isDeleted", false);	
+		//Get page result
+		IPageList pageList = roleService.listPage(pageParam);		
+		
+		//return params to response
+		modelMap.addAttribute("pageParam", pageParam);	
+		modelMap.addAttribute("pageList", pageList);	
+		
+    return new ModelAndView("/system/busPerm/index");	
+	
+	} 	
+	
+	/**
+	 * Add Page.
+	 * 
+	 * @return ModelAndView
+	 */	
+	@Func(funcCode="add", moduleCode="tenant.bizRole", enableLog=false)  
+	@RequestMapping
+	public ModelAndView add() 
+	{   		
+		PermissionTreeVo permissionTree = tenantBizService.findTenantPermissionTree(LoginUtils.getTenantId());
+		
+		return new ModelAndView("/system/busPerm/add").addObject("permissionTree", permissionTree);
+    }
+	
+	/**
+	 * Edit Page.
+	 * 
+	 * @param id 
+	 * 
+	 * @return ModelAndView
+	 */
+	@Func(funcCode="edit", moduleCode="tenant.bizRole", enableLog=false)  
+	@RequestMapping 
+	public ModelAndView edit(@RequestParam Integer id) 
+	{ 
+		Role role = roleService.load(id);
+		PermissionTreeVo permissionTree = tenantBizService.findTenantPermissionTree(LoginUtils.getTenantId(), id);
+        return new ModelAndView("/system/busPerm/edit").addObject("role", role).addObject("permissionTree", permissionTree);
+    }	
+    
+	/**
+	 * Show Page.
+	 * 
+	 * @param id 
+	 * 
+	 * @return ModelAndView
+	 */
+	@Func(funcCode="query", moduleCode="tenant.bizRole", enableLog=false)  
+	@RequestMapping 
+	public ModelAndView show(@RequestParam Integer id) 
+	{ 
+		Role role = roleService.load(id);		
+        return new ModelAndView("/system/busPerm/show").addObject("role", role);
+    }   
+    
+    /**
+	 * Save.
+	 * @param request 
+	 * @param modelMap
+	 * @throws Exception 
+	 */
+	@Func(funcCode="add",moduleCode="tenant.role")
+	@RequestMapping 
+	public void save(WebRequest request, ModelMap modelMap) throws Exception
+	{	
+		String[] permissionCodes = request.getParameterValues("permissionCodes");
+		
+		Role role = new Role();
+		BeanUtils.populate(role, request.getParameterMap());	
+		role.setTenantId(LoginUtils.getTenantId());
+		role.setStatus(RoleStatus.NORMAL);
+		roleService.save(role, permissionCodes);
+	}
+	
+	/**
+	 * Update.
+	 * @param request 
+	 * @param modelMap
+	 * @throws Exception 
+	 */
+	@Func(funcCode="edit",moduleCode="tenant.bizRole")
+	@RequestMapping 
+	public void update(WebRequest request, ModelMap modelMap) throws Exception
+	{
+		String[] permissionCodes = request.getParameterValues("permissionCodes");
+		
+		Role role = roleService.load(Utility.parseInt(request.getParameter("id")));
+		BeanUtils.populate(role, request.getParameterMap());
+		roleService.update(role, permissionCodes);
+	}
+	
+	/**
+	 * Delete.
+	 * @param id id
+	 * @throws Exception 
+	 */
+	@Func(funcCode="delete",moduleCode="tenant.bizRole")
+	@RequestMapping 
+	public void delete(@RequestParam Integer id) throws Exception
+	{
+		roleService.delete(id);
+	}
+
+	/**
+	 * Delete Batch.
+	 * @param id id
+	 * @throws Exception 
+	 */
+	@Func(funcCode="delete",moduleCode="tenant.bizRole")
+	@RequestMapping 
+	public void deleteBatch(@RequestParam(value="ids[]") Integer[] ids) throws Exception
+	{
+		roleService.deleteBatch(ids);
+	}
+	
+	/**
+	 * Resume.
+	 * @param id id
+	 * @throws Exception 
+	 */
+	@Func(funcCode="resume",moduleCode="tenant.bizRole")
+	@RequestMapping 
+	public void resume(@RequestParam Integer id) throws Exception
+	{
+		roleService.doResume(id);
+	}
+
+}
